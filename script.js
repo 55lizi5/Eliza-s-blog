69ed34f088dfa575d520c707893b03ad5393416d
55lizi5 <165923842+55lizi5@users.noreply.github.com>
2024-05-07 19:01:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openArticles() {
+    window.location.href = 'blog.html';
+}
+
+function openReviews() {
+    window.location.href = 'reviewsblog.html';
+}
+
+function openContact() {
+    window.location.href = 'contactblog.html';
+}
